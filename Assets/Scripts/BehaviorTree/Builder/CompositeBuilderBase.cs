34b8184de789250013131cdb63c2406c568a4c9f
43360540 <0183seanjean@gmail.com>
2026-03-25 09:49:56 +0800

    feat: basic bt tree builder completed

---
+++ Assets/Scripts/BehaviorTree/Builder/CompositeBuilderBase.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+
+namespace BehaviorTree
+{
+    public abstract class CompositeBuilderBase<TContext, TSelf> where TSelf : CompositeBuilderBase<TContext, TSelf>
+    {
+        protected readonly List<INode<TContext>> Children = new();
+        protected TSelf Self => (TSelf)this;
+
+        public TSelf Condition(ICondition<TContext> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            Children.Add(new ConditionLeaf<TContext>(predicate));
+            return Self;
+        }
+
+        public TSelf Condition(Func<TContext, float, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            Children.Add(new ConditionLeaf<TContext>(predicate));
+            return Self;
+        }
+
+        public TSelf Action(BTAction<TContext> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            Children.Add(new ActionLeaf<TContext>(action));
+            return Self;
+        }
+
+        public TSelf Action(ActionBundle<TContext> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            Children.Add(new ActionLeaf<TContext>(action));
+            return Self;
+        }
+
+        public TSelf Guard(ICondition<TContext> predicate, BTAction<TContext> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+            else if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+            
+            var actionLeaf = new ActionLeaf<TContext>(action);
+            Children.Add(new GuardDecorator<TContext>(predicate, actionLeaf));
+            return Self;
+        }
+
+        public TSelf Guard(Func<TContext, float, bool> predicate, BTAction<TContext> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+            else if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            var actionLeaf = new ActionLeaf<TContext>(action);
+            Children.Add(new GuardDecorator<TContext>(predicate, actionLeaf));
+            return Self;
+        }
+
+        public TSelf Selector(Action<SelectorCompositeBuilder<TContext>> buildAction)
+        {
+            if (buildAction == null)
+                throw new ArgumentNullException(nameof(buildAction));
+
+            var builder = new SelectorCompositeBuilder<TContext>();
+            buildAction(builder);
+            Children.Add(builder.Build());
+
+            return Self;
+        }
+
+        public TSelf Sequence(Action<SequenceCompositeBuilder<TContext>> buildAction)
+        {
+            if (buildAction == null)
+                throw new ArgumentNullException(nameof(buildAction));
+                
+            var builder = new SequenceCompositeBuilder<TContext>();
+            buildAction(builder);
+            Children.Add(builder.Build());
+
+            return Self;
+        }
+
+        protected abstract INode<TContext> CreateComposite(INode<TContext>[] children);
+
+        public INode<TContext> Build() => CreateComposite(Children.ToArray());
+    }
+}
---
+++ Assets/Scripts/BehaviorTree/Builder/RootBuilder.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace BehaviorTree
+{
+    public sealed class RootBuilder<TContext>
+    {
+        private INode<TContext> _root = null;
+
+        public void Selector(Action<SelectorCompositeBuilder<TContext>> buildAction)
+        {
+            if (_root != null)
+                throw new InvalidOperationException("Root node already set.");
+
+            var builder = new SelectorCompositeBuilder<TContext>();
+            buildAction(builder);
+
+            _root = builder.Build();
+        }
+
+        public void Sequence(Action<SequenceCompositeBuilder<TContext>> buildAction)
+        {
+            if (_root != null)
+                throw new InvalidOperationException("Root node already set.");
+
+            var builder = new SequenceCompositeBuilder<TContext>();
+            buildAction(builder);
+
+            _root = builder.Build();
+        }
+
+        public INode<TContext> Build() => _root;
+    }
+}
