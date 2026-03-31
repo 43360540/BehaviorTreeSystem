cc93e0c45c8dee9fea8f0883a9969cd23a00c7eb
43360540 <0183seanjean@gmail.com>
2026-03-31 21:15:07 +0800

    fix: naming error

--- Assets/Scripts/BehaviorTree/Unity/Sample/BTSample2.cs
+++ Assets/Scripts/BehaviorTree/Unity/Sample/BTSample2.cs
@@ -13,8 +13,8 @@ public class BTSample2 : MonoBTRunner<ContextSample>
     protected override INode<ContextSample> CreateTree()
     {
         INode<ContextSample> breakBranch =
-            BT<ContextSample>.Build(_ => _
-                .When(new IsTired(), canBreak => canBreak
+            BT<ContextSample>.Build(root => root
+                .When(new IsTired(), _ => _
                     .Parallel(brk => brk
                         .Do(new Pant())
                         .Do(new LookAround())
@@ -24,16 +24,16 @@ public class BTSample2 : MonoBTRunner<ContextSample>
             );
 
         INode<ContextSample> attackBranch =
-            BT<ContextSample>.Build(_ => _
-                .When(new IsTargetInRange(5f), canAttack => canAttack
+            BT<ContextSample>.Build(root => root
+                .When(new IsTargetInRange(5f), _ => _
                     .Do(new Attack())
                 )
             );
 
         INode<ContextSample> trackBranch =
-            BT<ContextSample>.Build(_ => _
-                .When(new IsTargetInRange(15f), track => track
-                    .Sequence(tracking => tracking
+            BT<ContextSample>.Build(root => root
+                .When(new IsTargetInRange(15f), _ => _
+                    .Sequence(track => track
                         .Do(new LookAround())
                         .Do(new TrackTarget())
                     )
@@ -42,11 +42,11 @@ public class BTSample2 : MonoBTRunner<ContextSample>
 
         INode<ContextSample> root =
             BT<ContextSample>.Build(root => root
-                .Selector(_ => _
+                .Selector(main => main
                     .AddChild(breakBranch)
                     .AddChild(attackBranch)
                     .AddChild(trackBranch)
-                    .Do(new Idle())
+                    .Do(new Idle()) // default
                 )
             );
 
