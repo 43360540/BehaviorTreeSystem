bce3898f6075a05e1efd53e5a656864705522039
43360540 <0183seanjean@gmail.com>
2026-04-13 23:02:44 +0800

    feat: add bool expected optional param into Check()

--- BehaviorTree.Core/Node/ConditionLeaf.cs
+++ BehaviorTree.Core/Node/ConditionLeaf.cs
@@ -5,68 +5,72 @@ namespace BehaviorTree
     public sealed class ConditionLeaf<TContext> : LeafBase<TContext>
     {
         private readonly ICondition<TContext> _condition;
+        private readonly bool _expected;
 
-        public ConditionLeaf(ICondition<TContext> condition, string? name = null) : base(name ?? condition.GetType().Name)
+        public ConditionLeaf(ICondition<TContext> condition, bool expected = true, string? name = null) : base(name ?? condition.GetType().Name)
         {
             _condition = condition ?? throw new ArgumentNullException(nameof(condition));
+            _expected = expected;
         }
 
         protected override NodeStatus OnTick(TContext ctx, float dt)
         {
-            return _condition.Evaluate(ctx, dt) ? NodeStatus.Success : NodeStatus.Failure;
+            if (_expected)
+                return _condition.Evaluate(ctx, dt) ? NodeStatus.Success : NodeStatus.Failure;
+            return _condition.Evaluate(ctx, dt) ? NodeStatus.Failure : NodeStatus.Success;
         }
     }
 
     public static class ConditionExtension
     {
         public static TSelf Check<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
-            ICondition<TContext> condition, string? name = null) =>
-            b.Add(new ConditionLeaf<TContext>(condition, name));
+            ICondition<TContext> condition, bool expected = true, string? name = null) =>
+            b.Add(new ConditionLeaf<TContext>(condition, expected, name));
 
         public static TSelf Check<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
-            Func<TContext, float, bool> predicate, string? name = "Check")
+            Func<TContext, float, bool> predicate, bool expected = true, string? name = "Check")
         {
             var qCondition = new QuickCondition<TContext>(predicate);
-            return b.Add(new ConditionLeaf<TContext>(qCondition, name));
-        }
-
-        public static TSelf Check<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
-            Func<float, bool> predicate, string? name = "Check")
-        {
-            var qCondition = new QuickCondition<TContext>(predicate);
-            return b.Add(new ConditionLeaf<TContext>(qCondition, name));
+            return b.Add(new ConditionLeaf<TContext>(qCondition, expected, name));
         }
 
         public static TSelf Check<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
-            Func<bool> predicate, string? name = "Check")
+            Func<float, bool> predicate, bool expected = true, string? name = "Check")
         {
             var qCondition = new QuickCondition<TContext>(predicate);
-            return b.Add(new ConditionLeaf<TContext>(qCondition, name));
+            return b.Add(new ConditionLeaf<TContext>(qCondition, expected, name));
+        }
+
+        public static TSelf Check<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
+            Func<bool> predicate, bool expected = true, string? name = "Check")
+        {
+            var qCondition = new QuickCondition<TContext>(predicate);
+            return b.Add(new ConditionLeaf<TContext>(qCondition, expected, name));
         }
 
         public static void Check<TContext>(this ISingleChild<TContext> b,
-            ICondition<TContext> condition, string? name = null) =>
-            b.Set(new ConditionLeaf<TContext>(condition, name));
+            ICondition<TContext> condition, bool expected = true, string? name = null) =>
+            b.Set(new ConditionLeaf<TContext>(condition, expected, name));
 
         public static void Check<TContext>(this ISingleChild<TContext> b,
-            Func<TContext, float, bool> predicate, string? name = "Check")
+            Func<TContext, float, bool> predicate, bool expected = true, string? name = "Check")
         {
             var qCondition = new QuickCondition<TContext>(predicate);
-            b.Set(new ConditionLeaf<TContext>(qCondition, name));
+            b.Set(new ConditionLeaf<TContext>(qCondition, expected, name));
         }
 
         public static void Check<TContext>(this ISingleChild<TContext> b,
-            Func<float, bool> predicate, string? name = "Check")
+            Func<float, bool> predicate, bool expected = true, string? name = "Check")
         {
             var qCondition = new QuickCondition<TContext>(predicate);
-            b.Set(new ConditionLeaf<TContext>(qCondition, name));
+            b.Set(new ConditionLeaf<TContext>(qCondition, expected, name));
         }
 
         public static void Check<TContext>(this ISingleChild<TContext> b,
-            Func<bool> predicate, string? name = "Check")
+            Func<bool> predicate, bool expected = true, string? name = "Check")
         {
             var qCondition = new QuickCondition<TContext>(predicate);
-            b.Set(new ConditionLeaf<TContext>(qCondition, name));
+            b.Set(new ConditionLeaf<TContext>(qCondition, expected, name));
         }
     }
 }
