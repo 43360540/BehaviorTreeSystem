ddce7396cc72906e3f57aa1dfcf784b6a0104600
43360540 <0183seanjean@gmail.com>
2026-04-15 02:18:16 +0800

    feat: Add overload for unlimited RepeaterDecorator

--- BehaviorTree.Core/Node/RepeaterDecorator.cs
+++ BehaviorTree.Core/Node/RepeaterDecorator.cs
@@ -4,7 +4,7 @@ namespace BehaviorTree
 {
     public sealed class RepeaterDecorator<TContext> : DecoratorBase<TContext>
     {
-        private readonly int _times;
+        private readonly int? _times;
         private int _currentTimes = 0;
 
         public RepeaterDecorator(int times, INode<TContext> child, string? name = null) : base(child, name ?? "Repeater")
@@ -15,13 +15,20 @@ namespace BehaviorTree
             _times = times;
         }
 
+        public RepeaterDecorator(INode<TContext> child, string? name = null) : base(child, name ?? "Repeater")
+        {
+            _times = null;
+        }
+
         protected override NodeStatus OnTick(TContext ctx, float dt)
         {
             var status = Child.Tick(ctx, dt);
             if (status == NodeStatus.Success)
             {
                 _currentTimes++;
-                if (_currentTimes >= _times)
+                if (_times == null)
+                    return NodeStatus.Running;
+                else if (_currentTimes >= _times)
                     return NodeStatus.Success;
 
                 return NodeStatus.Running;
@@ -46,12 +53,28 @@ namespace BehaviorTree
             return b.Add(builder.Build((t, c, n) => new RepeaterDecorator<TContext>(t, c, n)));
         }
 
+        public static TSelf Repeater<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
+            Action<ISingleChild<TContext>> buildAction, string? name = null)
+        {
+            var builder = new DecoratorBuilder<TContext>(name);
+            buildAction(builder);
+            return b.Add(builder.Build((c, n) => new RepeaterDecorator<TContext>(c, n)));
+        }
+
         public static void Repeater<TContext>(this ISingleChild<TContext> b,
             int times, Action<ISingleChild<TContext>> buildAction, string? name = null)
         {
             var builder = new DecoratorBuilder<int, TContext>(times, name);
             buildAction(builder);
             b.Set(builder.Build((t, c, n) => new RepeaterDecorator<TContext>(t, c, n)));
+        }
+
+        public static void Repeater<TContext>(this ISingleChild<TContext> b,
+            Action<ISingleChild<TContext>> buildAction, string? name = null)
+        {
+            var builder = new DecoratorBuilder<TContext>(name);
+            buildAction(builder);
+            b.Set(builder.Build((c, n) => new RepeaterDecorator<TContext>(c, n)));
         }
     }
 }
