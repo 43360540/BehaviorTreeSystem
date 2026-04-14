076c95d2ea9cefcaf5d0d19d8cfd3d7d6dfa80e8
43360540 <0183seanjean@gmail.com>
2026-04-15 00:45:34 +0800

    fix: CoolDownDecorator logic error

--- BehaviorTree.Core/Node/CoolDownDecorator.cs
+++ BehaviorTree.Core/Node/CoolDownDecorator.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace BehaviorTree
 {
-    public sealed class CollDownDecorator<TContext> : DecoratorBase<TContext>, IGuard<TContext>
+    public sealed class CoolDownDecorator<TContext> : DecoratorBase<TContext>, IGuard<TContext>
     {
         private float _elapsed;
         private float _cd;
@@ -16,12 +16,13 @@ namespace BehaviorTree
                 _elapsed += dt;
         }
 
-        public CollDownDecorator(float coolDown, INode<TContext> child, string? name = null) : base(child, name ?? "CoolDown")
+        public CoolDownDecorator(float coolDown, INode<TContext> child, string? name = null) : base(child, name ?? "CoolDown")
         {
             if (coolDown <= 0)
                 throw new InvalidOperationException("CoolDownDecorator.coolDown cannot be below 0.");
             
             _cd = coolDown;
+            _elapsed = coolDown + 1e-4f;
         }
 
         public bool CanEnter(TContext ctx, float dt)
@@ -42,16 +43,27 @@ namespace BehaviorTree
             }
             return Child.Tick(ctx, dt);
         }
+
+        protected override void OnStop(TContext ctx, NodeStatus stopStatus)
+        {
+            if (stopStatus == NodeStatus.Success)
+                _elapsed = 0;
+        }
+
+        protected override void OnReset()
+        {
+            _cachedResult = null;
+        }
     }
 
-    public static class CollDownExtension
+    public static class CoolDownExtension
     {
         public static TSelf CoolDown<TSelf, TContext>(this IMultiChildren<TSelf, TContext> b,
             float coolDown, Action<ISingleChild<TContext>> buildAction, string? name = null)
         {
             var builder = new DecoratorBuilder<float, TContext>(coolDown, name);
             buildAction(builder);
-            return b.Add(builder.Build((cd, c, n) => new CollDownDecorator<TContext>(cd, c, n)));
+            return b.Add(builder.Build((cd, c, n) => new CoolDownDecorator<TContext>(cd, c, n)));
         }
 
         public static void CoolDown<TContext>(this ISingleChild<TContext> b,
@@ -59,7 +71,7 @@ namespace BehaviorTree
         {
             var builder = new DecoratorBuilder<float, TContext>(coolDown, name);
             buildAction(builder);
-            b.Set(builder.Build((cd, c, n) => new CollDownDecorator<TContext>(cd, c, n)));
+            b.Set(builder.Build((cd, c, n) => new CoolDownDecorator<TContext>(cd, c, n)));
         }
     }
 }
